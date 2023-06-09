b46477bf10e8211699d3f9babc5df6435211a738
R-aliyeva <rufanefa@code.edu.az>
2023-06-09 22:54:17 +0400

    addded

---
+++ Datetime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Datetime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+       string date=Console.ReadLine();
+            try { DateTime dateTime = DateTime.ParseExact(date, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                Console.WriteLine(dateTime);
+            }
+            catch (Exception e)
+            { 
+                Console.WriteLine(e.Message);
+            }
+            
+            
+        }
+    }
+}
